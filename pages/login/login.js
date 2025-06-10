c95d6d45ee7c0494fb60d865001610b12b1303b2
sosofos <dantassofia84@gmail.com>
2025-06-10 11:25:00 -0300

    dia 1006

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     };
     
     try {
-        const response = await fetch('http://localhost:3000/usuarios/login', {
+        const response = await fetch('http://localhost:3000/usuarios/login', { 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,16 +18,13 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         const data = await response.json();
         
         if (response.ok) {
-            // Salvar dados do usuário no localStorage (para demonstração)
-            localStorage.setItem('token', JSON.stringify(data.token));
-            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('token', JSON.stringify(data.token)); 
+            localStorage.setItem('user', JSON.stringify(data.user)); 
             
-            
-            // Redirecionar para a página principal (após login bem-sucedido)
             alert('Login realizado com sucesso!');
-            window.location.href = '/dashboard/dashboard.html';  // Página após login
+            window.location.href = '../dashboard/dashboard.html';  
         } else {
-            alert(`Erro:${data.message || 'Credenciais inválidas'}`);
+            alert(`Erro: ${data.message || 'Credenciais inválidas'}`);
         }
     } catch (error) {
         console.error('Erro:', error);
